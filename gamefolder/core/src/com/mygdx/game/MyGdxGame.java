6b307c3d48c01a06ff74331f9c1899e3263ded15
Emily Pascetta <egp1018@usnh.edu>
2024-03-04 13:49:29 -0500

    Test to verify I cloned the repository correctly, only a comment was added

--- gamefolder/core/src/com/mygdx/game/MyGdxGame.java
+++ gamefolder/core/src/com/mygdx/game/MyGdxGame.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class MyGdxGame extends ApplicationAdapter {
+	//THIS IS A TEST COMMENT
 	SpriteBatch batch;
 	Texture img;
 	
