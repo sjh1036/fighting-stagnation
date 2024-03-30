03b865fe142f62f7978e85b80e3204fca183d9c7
Emily Pascetta <egp1018@usnh.edu>
2024-03-30 16:58:24 -0400

    Rough draft of main menu screen     -Music     -Background Image     -New Bitmap font
    Tiled map rendering on start
        -Map moves from user input with random sprite
      **Platforms do not yet have properties**

---
+++ assets/Objects.tsx
@@ -0,0 +1,49 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Objects" tilewidth="64" tileheight="53" tilecount="15" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="89">
+  <image width="64" height="41" source="PNG/Objects/fence.png"/>
+ </tile>
+ <tile id="90">
+  <image width="16" height="9" source="PNG/Objects/grass1.png"/>
+ </tile>
+ <tile id="91">
+  <image width="16" height="10" source="PNG/Objects/grass2.png"/>
+ </tile>
+ <tile id="92">
+  <image width="16" height="12" source="PNG/Objects/grass3.png"/>
+ </tile>
+ <tile id="93">
+  <image width="32" height="22" source="PNG/Objects/pink_bush1.png"/>
+ </tile>
+ <tile id="95">
+  <image width="32" height="21" source="PNG/Objects/rectangle1.png"/>
+ </tile>
+ <tile id="96">
+  <image width="64" height="42" source="PNG/Objects/rocks1_1 - Copy.png"/>
+ </tile>
+ <tile id="97">
+  <image width="32" height="31" source="PNG/Objects/rocks1_2 - Copy.png"/>
+ </tile>
+ <tile id="98">
+  <image width="64" height="53" source="PNG/Objects/rocks1_3.png"/>
+ </tile>
+ <tile id="99">
+  <image width="32" height="20" source="PNG/Objects/rocks1_4 - Copy.png"/>
+ </tile>
+ <tile id="100">
+  <image width="64" height="53" source="PNG/Objects/rocks1_5 - Copy.png"/>
+ </tile>
+ <tile id="101">
+  <image width="32" height="32" source="PNG/Objects/rocks2_3 - Copy - Copy.png"/>
+ </tile>
+ <tile id="102">
+  <image width="32" height="32" source="PNG/Objects/rocks2_3.png"/>
+ </tile>
+ <tile id="105">
+  <image width="48" height="27" source="PNG/Objects/Antler.png"/>
+ </tile>
+ <tile id="107">
+  <image width="32" height="30" source="PNG/Objects/pink_bush2.png"/>
+ </tile>
+</tileset>
